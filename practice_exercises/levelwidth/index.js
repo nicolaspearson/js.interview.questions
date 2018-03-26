9da795331ccdc263a01e87560dde99621fb166e4
Nicolas Pearson <nic.s.pearson@gmail.com>
2018-03-26 06:23:09 +0200

    Practice Session

--- practice_exercises/levelwidth/index.js
+++ practice_exercises/levelwidth/index.js
@@ -11,6 +11,28 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  if (!root) {
+    return [];
+  }
+  const levels = [];
+  const array = [{ row: 0, node: root }];
+  const sumObjects = { 0: 1 };
+
+  while (array.length > 0) {
+    const element = array.shift();
+    for (let child of element.node.children) {
+      const row = element.row + 1;
+      array.push({ row, node: child });
+      sumObjects[row] = (sumObjects[row] || 0) + 1;
+    }
+  }
+
+  const result = [];
+  for (key in sumObjects) {
+    result.push(sumObjects[key]);
+  }
+  return result;
+}
 
 module.exports = levelWidth;
