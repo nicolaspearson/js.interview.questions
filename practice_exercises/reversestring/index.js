fb7b38de6609bd6df78a7e5ac3f4ef7572f2af11
Nicolas Pearson <nic.s.pearson@gmail.com>
2019-02-27 18:33:04 +0200

    Practice

--- practice_exercises/reversestring/index.js
+++ practice_exercises/reversestring/index.js
@@ -6,6 +6,16 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((acc, cur) => {
+        return acc = cur + acc;
+    }, '');
+    // return str.split('').reverse().join('');
+    // let word = '';
+    // for (let i = str.length -1; i >= 0; i--) {
+    //     word += str[i];
+    // }
+    // return word;
+}
 
 module.exports = reverse;
