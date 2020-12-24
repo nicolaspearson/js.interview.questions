6d1432d2e22f0df970cf810c3730c27087854290
Nic Pearson <nic.s.pearson@gmail.com>
2020-12-24 14:58:10 +0100

    chore: practising

--- practice_exercises/reverseint/index.js
+++ practice_exercises/reverseint/index.js
@@ -8,6 +8,25 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  return parseInt(n.toString().split('').reverse().join('')) * Math.sign(n);
+}
+
+// function reverseInt(n) {
+//   let result = '';
+//   for (let item of n.toString()) {
+//     if (item !== '-') {
+//       result = item + result;
+//     }
+//   }
+//   if (n < 0) return parseInt(result) * -1;
+//   return parseInt(result);
+// }
+
+// function reverseInt(n) {
+//   const result = parseInt(n.toString().replace(/[^\w]/g, '').split('').reverse().join(''));
+//   if (n < 0) return result * -1;
+//   return result;
+// }
 
 module.exports = reverseInt;
